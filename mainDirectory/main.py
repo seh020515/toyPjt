3e3fc057c20cb25441384d3ec7a78268115da2c0
kyle <kylego.studio@gmail.com>
2026-05-20 21:41:18 +0900

    20260520 edit_member task done

--- mainDirectory/main.py
+++ mainDirectory/main.py
@@ -1,3 +1,4 @@
+from edit_member import inject_dummy_data, editInfo
 flag = True
 
 SIGN_UP                 = 1
@@ -9,6 +10,8 @@ SYSTEM_SHUTDOWN         = 99
 #더미 데이터 넣을 공간------------
 DEV_MOD = True
 members = {}
+if DEV_MOD:
+    inject_dummy_data(members)
 #------------------------------
 
 
@@ -21,9 +24,6 @@ def signIn():
     pass
 
 def printInfo():
-    pass
-
-def editInfo():
     pass
 
 def getMenu():
@@ -43,7 +43,7 @@ while flag:
         printInfo()
     
     elif userSelectedMenuNum == EDIT_INFO:
-        editInfo()
+        editInfo(members)   #members에 전달
     
     elif userSelectedMenuNum == SYSTEM_SHUTDOWN:
         flag = False
