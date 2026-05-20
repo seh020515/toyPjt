22881e9d373c6a4770950f73e5503846a81fad0a
seh020515 <seh020515@gmail.com>
2026-05-20 16:55:02 +0900

    start pjt

---
+++ mainDirectory/main.py
@@ -0,0 +1,50 @@
+flag = True
+
+SIGN_UP                 = 1
+SIGN_IN                 = 2
+PRINT_INFO              = 3
+EDIT_INFO               = 4
+SYSTEM_SHUTDOWN         = 99
+
+#더미 데이터 넣을 공간------------
+DEV_MOD = True
+members = {}
+#------------------------------
+
+
+
+#함수
+def signUp():
+    pass
+
+def signIn():
+    pass
+
+def printInfo():
+    pass
+
+def editInfo():
+    pass
+
+def getMenu():
+    menu = int(input('1.회원가입 2.로그인 3.회원 조회 4.회원정보 수정 99.종료 : '))
+    return menu
+
+while flag:
+    userSelectedMenuNum = getMenu()
+    
+    if userSelectedMenuNum == SIGN_UP:
+        signUp()
+    
+    elif userSelectedMenuNum == SIGN_IN:
+        signIn()
+    
+    elif userSelectedMenuNum == PRINT_INFO:
+        printInfo()
+    
+    elif userSelectedMenuNum == EDIT_INFO:
+        editInfo()
+    
+    elif userSelectedMenuNum == SYSTEM_SHUTDOWN:
+        flag = False
+        print('안녕히가십시오.')  #프로그램 종료 시에 출력 멘트
