dde73b46d40144617a429b0d41543222f9e8470e
heechuchu <s102524@gmail.com>
2026-05-21 09:35:34 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from auth import login, logout
+
+
+def show_menu():
+
+    print("\n===== 메뉴 =====")
+    print("1. 로그인")
+    print("2. 로그아웃")
+    print("3. 종료")
+
+
+def main():
+
+    while True:
+
+        show_menu()
+
+        menu = input("메뉴 선택: ")
+
+        if menu == "1":
+
+            result = login()
+
+            if result == False:
+                break
+
+        elif menu == "2":
+            logout()
+
+        elif menu == "3":
+            print("프로그램 종료")
+            break
+
+        else:
+            print("잘못된 입력")
+
+
+main()
