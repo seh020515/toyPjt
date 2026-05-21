fcfc6b1dbdca3e67dc2b4aebef870d9b2be21c33
giljungyu62 <giljungyu62>
2026-05-21 11:03:53 +0900

    20260520 done

---
+++ signup.py
@@ -0,0 +1,43 @@
+# members = {}
+# # 회원가입 기능
+# def signUp():
+#     global members
+#     print('\n[회원가입]')
+# # ID 입력 및 중복 체크
+# while True:
+#     user_id = input('회원 ID 입력: ')
+
+#     if user_id in members:
+#         print("이미 사용중인 ID입니다. 다시 입력하세요.")
+#     else:
+#         break 
+# # 비밀번호 지정 함수
+# def SetPassword():
+    
+#     while True:
+        
+#         Pw = input('회원 비밀번호 입력: ')   
+# # 이메일 형식 검사
+# while True:
+#     user_email = input('회원 Email 입력: ')
+#     if "@" in user_email and "." in user_email:
+#         break
+#     else:
+#         print("올바른 이메일 형식이 아닙니다. 다시 입력하세요.")
+# # 전화번호 '-' 자동 제거
+# user_phone = input('회원 phone 입력: ')
+# user_phone = user_phone.replace('-', "")
+
+a = 10
+b = 1.2 
+c= 'hello'
+
+a = float(a)
+b = int(b)
+
+print(type(a))
+print(type(b))
+print(type(c))
+ 
+
+
